f3e6d7ebb61a1b02ca93061ce4c4ebe7660da371
Jared Hasen-Klein <GitHub@jaredhk.com>
2026-03-03 09:35:28 -0800

    Fix formatting in requiredCourses for Safety Manager

--- scripts/certification.js
+++ scripts/certification.js
@@ -43,7 +43,7 @@ const PROGRAM_CONFIGS = {
         },
         'Safety Manager': {
             requiredRoles: ['Safety Manager'],
-            requiredCourses: ['FIRST Robotics Competition Safety Manager',]
+            requiredCourses: ['FIRST Robotics Competition Safety Manager']
         },
         'Pit Admin Supervisor': {
             requiredRoles: ['Pit Administration Supervisor'],
